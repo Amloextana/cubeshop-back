9d3b7e3d40c0126a7db8071fea5b34c90a882b40
Александр <alex.otsel@yandex.ru>
2023-09-17 17:28:29 +0300

    1.0 lab2 start

--- bmstu/CubeSat/models.py
+++ bmstu/CubeSat/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+
+class Customer(User):
+    pass
+
+
+class Employer(User):
+
+    class PositionChoices(models.TextChoices):
+        JUNIOR_MANAGER = 'Junior manager'
+        SENIOR_MANAGER = 'Senior manager'
+
+    position = models.CharField(max_length=30, choices=PositionChoices.choices, default=PositionChoices.JUNIOR_MANAGER)
+    is_manager = models.BooleanField(default=True)
+
+
+class Request(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        PENDING = 'Pending'
+        IN_PROGRESS = 'In Progress'
+        COMPLETED = 'Completed'
+        CANCELLED = 'Cancelled'
+        DELETED = 'Deleted'
+
+    status = models.CharField(max_length=20, choices=StatusChoices.choices, default=StatusChoices.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    formed_at = models.DateTimeField(null=True, blank=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    moderator = models.ForeignKey(Employer, on_delete=models.SET_NULL, null=True, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+
+class Property(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+
+
+class ProductProperty(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    value = models.CharField(max_length=255)
+
+
+class RequestsToDetails(models.Model):
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+    detail = models.ForeignKey(Product, on_delete=models.CASCADE)
--- bmstu/bmstu/settings.py
+++ bmstu/bmstu/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'CubeSat'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'bmstu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CubeSat',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
